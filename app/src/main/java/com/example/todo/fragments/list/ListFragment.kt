1634709f24e5671a191063a653d60a1674eb1168
AnujaPednekar <anujapednekar10@gmail.com>
2020-08-26 11:00:29 +0530

    First commit

--- app/src/main/java/com/example/todo/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/todo/fragments/list/ListFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.todo.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -17,4 +19,10 @@ class ListFragment : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        noDataFab.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+    }
 }
